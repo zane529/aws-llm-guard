66cad67a9e25c8ef6dcfc4c282ce750fd8a139e2
Chen <189380064829@163.com>
2024-12-05 13:39:16 +0800

    add competitors

--- toxicity.py
+++ toxicity.py
@@ -1,7 +1,13 @@
 from llm_guard.input_scanners import Toxicity
 from llm_guard.input_scanners.toxicity import MatchType
+from llm_guard.input_scanners.toxicity import DEFAULT_MODEL as TOXICITY_MODEL
 
-scanner = Toxicity(threshold=0.5, match_type=MatchType.SENTENCE)
+TOXICITY_MODEL.path = "/home/ubuntu/llm-guard/model/dpo-toxic-zh"
+TOXICITY_MODEL.kwargs["local_files_only"] = True
+
+scanner = Toxicity(model=TOXICITY_MODEL,threshold=0.5, match_type=MatchType.SENTENCE)
+
+
 sanitized_prompt, is_valid, risk_score = scanner.scan("我要杀了你")
 print(sanitized_prompt)
 print(is_valid)
