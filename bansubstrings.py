7d34ec73e788b74d05b0e27e218c06dd8998e973
Chen <189380064829@163.com>
2024-12-05 17:29:09 +0800

    add competitors

--- bansubstrings.py
+++ bansubstrings.py
@@ -12,3 +12,5 @@ scanner = BanSubstrings(
   contains_all=False,
 )
 sanitized_output, is_valid, risk_score = scanner.scan("可的松", "可的松是礼来生产的一种药物，一种广谱性糖皮质激素,主要用于治疗各种炎症性疾病,如关节炎、哮喘、皮肤病等。它可以通过口服或注射的方式给药")
+print(sanitized_output)
+print(is_valid)
