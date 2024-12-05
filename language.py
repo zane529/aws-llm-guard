24a752bc7d708c34d5d850de341bb4b3c378fbcd
Chen <189380064829@163.com>
2024-12-05 17:47:48 +0800

    add competitors

---
+++ language.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+from llm_guard.output_scanners import Language
+from llm_guard.input_scanners.language import MatchType
+
+scanner = Language(valid_languages=["en"], match_type=MatchType.FULL)  # Add other valid language codes (ISO 639-1) as needed
+sanitized_output, is_valid, risk_score = scanner.scan("中文测试", "你的 idea 很好，但是是有问题的")
