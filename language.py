72456c29875165e914766c85b2e0e2a0697296ea
Chen <189380064829@163.com>
2024-12-05 17:49:33 +0800

    add competitors

--- language.py
+++ language.py
@@ -6,3 +6,5 @@ from llm_guard.input_scanners.language import MatchType
 
 scanner = Language(valid_languages=["en"], match_type=MatchType.FULL)  # Add other valid language codes (ISO 639-1) as needed
 sanitized_output, is_valid, risk_score = scanner.scan("中文测试", "你的 idea 很好，但是是有问题的")
+print(sanitized_output)
+print(is_valid)
