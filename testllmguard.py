54a164e7e3f74f02056eb5acb3dd6905aff801d7
Chen <189380064829@163.com>
2024-12-05 12:39:46 +0800

    first

--- testllmguard.py
+++ testllmguard.py
@@ -10,7 +10,7 @@ from llm_guard.input_scanners.anonymize_helpers import BERT_LARGE_NER_CONF
 from llm_guard.input_scanners.anonymize_helpers import BERT_ZH_NER_CONF
 
 scanner = Anonymize(vault, preamble="Insert before prompt", allowed_names=["John Doe"], hidden_names=["Test LLC"],
-                    recognizer_conf=BERT_ZH_NER_CONF, language="zh")
-sanitized_prompt, is_valid, risk_score = scanner.scan("我叫王大，喜欢去旺角餐厅吃牛角包, 今年买了阿里巴巴的股票，亏得舅老爷的裤衩都没了，我的手机号是13587677888")
+                    recognizer_conf=BERT_ZH_NER_CONF, language="zh", use_onnx=True)
+sanitized_prompt, is_valid, risk_score = scanner.scan("我叫王大，喜欢去旺角餐厅吃牛角包, 今年买了阿里巴巴的股票，亏得舅老爷的裤衩都没了，我的手机号是 13587677888")
 print(sanitized_prompt)
 print(is_valid)
