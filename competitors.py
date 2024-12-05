d3bd4ab04ecb75e88b5a4303d19c7171e8d978ac
Chen <189380064829@163.com>
2024-12-05 13:15:48 +0800

    add competitors

---
+++ competitors.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from llm_guard.input_scanners import BanCompetitors
+
+competitor_list = ["礼来", "Lilly"]  # Extensive list of competitors
+scanner = BanCompetitors(competitors=competitor_list, redact=False, threshold=0.5)
+sanitized_prompt, is_valid, risk_score = scanner.scan("帮我介绍一下礼来这家公司")
